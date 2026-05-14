793443a77d58034a0128f96eb4f01d0cd1cf2f9c
Israel <israel@MacBook-Pro-3.local>
2026-05-14 17:10:11 -0400

    Add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+INVENTARIO_SHEET_URL = "https://docs.google.com/spreadsheets/d/1KIiqr2jdtBaL8GdQZrK-_xjj2Hpu9riDweNcwQVIO5A/edit"
+INVENTARIO_HOJA_NAME = "Inventario"
+ 
+# URL de la Google Sheet donde se guardan los pedidos
+PEDIDOS_SHEET_URL = "https://docs.google.com/spreadsheets/d/1KIiqr2jdtBaL8GdQZrK-_xjj2Hpu9riDweNcwQVIO5A/edit"
+PEDIDOS_HOJA_NAME = "Pedidos"
+ 
